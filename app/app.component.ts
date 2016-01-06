b5c47975665c7b21e55a3a2398bbe1a3764debae
Jorge Casariego <jorgekasa@gmail.com>
2016-01-06 18:24:35 -0300

    Implementación input en app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,29 @@
 import {Component} from 'angular2/core';
+
+interface Persona {
+	id: number;
+	nombre: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>{{titulo}}</h1><h2>{{nombre}} details!</h2>'
+    template: 	`<h1>{{titulo}}</h1> 
+    			 <h2>{{persona.nombre}} detalle!</h2> 
+
+    			 <div><label>ID:     </label>{{persona.id}}</div>
+				 <div>
+				 	<label>Nombre: </label>
+				 	<div><input [(ngModel)]="persona.nombre" placeholder="nombre"></div>
+				 </div>
+				`;
+
 })
+
 export class AppComponent { 
 	public titulo = 'Personas';
-	public nombre = 'Jorge';
+
+	public persona: Persona = {
+		id: 1,
+		nombre: "Jorge Casariego"
+	};
 }
